653b15e720de92e0011749ef28160d1dbf3427ae
rachelgray7448 <rachel.gray7448@gmail.com>
2022-02-16 22:56:32 -0500

    adding loops for enemys

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,8 +17,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as th enemy-robot is alive
+    while(enemyHealth > 0) {
 
     var promptFight = window.prompt("would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -76,8 +76,11 @@ var fight = function(enemyName) {
     else { 
         window.alert("You need to choose a valid option. Try again!");
     }
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
