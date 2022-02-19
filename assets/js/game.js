ad8f3ce14088dbef4070eceede99c7f75d3cd5e0
Rachel Gray <rachel.gray7448@gmail.com>
2022-02-18 19:02:07 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,14 +31,16 @@ var fight = function(enemyName) {
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip the fight. Goodbye!");
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
     }
 }
 
     // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
+    //generate random damage value based on player's attack power
+    var damage = randomNumber (playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -55,7 +57,8 @@ var fight = function(enemyName) {
     }
 
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
 
     // Log a resulting message to the console so we know that it worked.
     console.log(
@@ -73,6 +76,13 @@ var fight = function(enemyName) {
     }
 };
 
+// function to generate a random numeric value
+var randomNumber = function(min,max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+}
+
 
 //function to start a new game
 var startGame = function() {
@@ -85,7 +95,7 @@ var startGame = function() {
         if (playerHealth >0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i+1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             
             // if player still alive and we're not at the last enemy in the array
